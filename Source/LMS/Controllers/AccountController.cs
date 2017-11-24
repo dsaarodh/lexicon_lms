4c194506b991d3a33105eaaac42ae0ae2a46dcd6
Daniel Saar Odhammer <daniel.saarodhammer@gmail.com>
2017-11-24 13:45:42 +0100

    * Moved scaffolded files to better folders. * Enabled migrations

--- Source/LMS/Controllers/AccountController.cs
+++ Source/LMS/Controllers/AccountController.cs
@@ -8,7 +8,10 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using LMS.Models;
+using LMS.DataAccess;
+using LMS.ViewModels;
+using LMS.Models.Identity;
+using LMS.ViewModels.Identity;
 
 namespace LMS.Controllers
 {
