aa78be94c09d658843b8c068d23052b821ec1784
Daniel Saar Odhammer <daniel.saarodhammer@gmail.com>
2017-11-28 14:56:19 +0100

    * Now making sure Db is created if nonexistant.

--- Source/LMS/Startup.cs
+++ Source/LMS/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Owin;
+﻿using LMS.DataAccess;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(LMS.Startup))]
@@ -8,6 +9,8 @@ namespace LMS
     {
         public void Configuration(IAppBuilder app)
         {
+			new ApplicationDbContext().Database.CreateIfNotExists();
+
             ConfigureAuth(app);
         }
     }
