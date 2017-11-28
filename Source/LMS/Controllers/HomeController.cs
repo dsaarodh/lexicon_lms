130a7fc65be04df9e883471fd9d9415207e66848
Thomas Palestig <tpalestig@gmail.com>
2017-11-28 16:31:07 +0100

    Display Course Updated all NuGet packages Fixed design of index tabs-frame

--- Source/LMS/Controllers/HomeController.cs
+++ Source/LMS/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using LMS.Models;
+using LMS.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,12 +11,17 @@ namespace LMS.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
-		{
-			return View();
-		}
+        [Authorize(Roles = "Teacher, Student")]
+        public ActionResult Index()
+        {
+            var schedule = new ScheduleViewModels();
+            CultureInfo cultureInfo = new CultureInfo("sv-SE");
+            schedule.Calendar = cultureInfo.Calendar;            
+            schedule.Course = new CourseModel() { Name = "Test Course" };
+            return View(schedule);
+        }
 
-		public ActionResult About()
+        public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
 
@@ -26,5 +34,10 @@ namespace LMS.Controllers
 
 			return View();
 		}
+
+        public PartialViewResult Module()
+        {
+            return PartialView("_Module");
+        }
 	}
 }
---
+++ Source/LMS/ViewModels/SheduleViewModels.cs
@@ -0,0 +1,16 @@
+﻿using LMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace LMS.ViewModels
+{
+    public class ScheduleViewModels
+    {
+        public Calendar Calendar { get; set; }
+        public CourseModel Course { get; set; }
+        public DateTime FirstDayOfWeek { get => DateTime.Today.AddDays(-(DateTime.Today.DayOfWeek - new CultureInfo("sv-SE").DateTimeFormat.FirstDayOfWeek)); }
+    }
+}
