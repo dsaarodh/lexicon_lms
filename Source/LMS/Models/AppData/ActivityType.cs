59bd8956b3ef9c78d324d1dad4684e02996e9136
Tomas Lanquist <tomas.lexicon@gmail.com>
2017-12-13 16:31:26 +0100

    Change of language. Add ActivityType to DeleteActivity. Additional minor fixes.

--- Source/LMS/Models/AppData/ActivityType.cs
+++ Source/LMS/Models/AppData/ActivityType.cs
@@ -1,8 +1,12 @@
-﻿namespace LMS.Models.AppData
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Models.AppData
 {
 	public class ActivityType
     {
         public int Id { get; set; }
+
+        [Display(Name = "Typ")]
         public string Name { get; set; }
     }
 }
